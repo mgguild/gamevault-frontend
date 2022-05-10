514c96671e9cc745514a563ff8afc021057bb06e
Jambs <salvadorjr.amba@gmail.com>
2022-05-10 17:17:26 +0800

    put styled components to sparate file(styled.tsx)

--- src/views/Farms/NewUI/FarmPage.tsx
+++ src/views/Farms/NewUI/FarmPage.tsx
@@ -53,6 +53,14 @@ import { getBscScanAddressUrl } from '../../../utils/bscscan'
 import { Cards2, Card2Container, TokenLogo, Badge, LinearBG, PageContainer } from '../components/FarmCards/styles'
 import { RenderSocials } from '../../../components/Launchpad/Logo'
 import InputComponent from './InputComponent'
+import {
+  FlexC,
+  ButtonSM,
+  StatCard,
+  Stats,
+  TableStyle,
+  ChartStyle
+} from './styled'
 
 BigNumber.config({
   DECIMAL_PLACES: 4,
@@ -63,81 +71,6 @@ BigNumber.config({
   },
 })
 
-const FlexC = styled(Flex)`
-  padding: 2.5rem;
-  flex-flow column wrap;
-  row-gap: 1rem;
-  align-content: center;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-`
-
-const ButtonSM = styled(Button)`
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  height: 2.5rem;
-  border-radius: 4px;
-`
-
-const StatCard = styled(Flex)`
-  padding: 1rem;
-  flex-flow: column;
-  row-gap: 1rem;
-  background-color: ${({ theme }) => theme.colors.MGG_mainBG};
-  text-align: start;
-  min-width: 21rem;
-  flex: 1;
-`
-
-const Stats = styled(Flex)`
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem 1rem;
-  background-color: ${({ theme }) => theme.colors.MGG_container};
-  min-width: 11rem;
-`
-
-const TableStyle = styled.div`
-  display: flex;
-  position: relative;
-  min-height: 15rem;
-  width: 100%;
-  padding: 1rem;
-  overflow: auto;
-  background-color: ${({ theme }) => theme.colors.MGG_mainBG};
-  z-index: 2;
-
-  table {
-    width: 99%;
-    position: absolute;
-
-    th {
-      text-align: start;
-      margin: 0;
-      padding: 0.5rem;
-    }
-    td {
-      text-align: start;
-      margin: 0;
-      padding: 0.5rem;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-  }
-`
-
-const ChartStyle = styled(Flex)`
-  width: 100%;
-  max-width: 100%;
-  background-color: ${({ theme }) => theme.colors.MGG_mainBG};
-  padding: 1rem;
-  z-index: 3;
-  .apexcharts-text {
-    fill: ${({ theme }) => theme.colors.text};
-  }
-`
 
 type Series = {
   name: string

a706121f952f827d141b66a33c3f919c9048aca9
Jambs <salvadorjr.amba@gmail.com>
2022-05-10 19:07:09 +0800

    add gamfi type config

--- src/views/Gamefi/components/config.ts
+++ src/views/Gamefi/components/config.ts
@@ -1,7 +1,10 @@
 import BigNumber from 'bignumber.js'
+import { Token } from 'config/constants/types'
 import { Farm } from 'state/types'
 
 export interface FarmWithStakedValue extends Farm {
   apr?: number
   liquidity?: BigNumber
+  stakeTiers?: any
+  stakingToken?: Token
 }
